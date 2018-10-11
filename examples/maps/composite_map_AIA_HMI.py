5c286cd82d6aa1e0759346f35a070f8da5d124d8
Andre Chicrala <andrechicrala@northumbria.ac.uk>
2018-10-11 17:48:16 +0100

    Adjusts on the comments.

--- examples/maps/composite_map_AIA_HMI.py
+++ examples/maps/composite_map_AIA_HMI.py
@@ -22,17 +22,14 @@ hmi_map = sunpy.map.Map(sunpy.data.sample.HMI_LOS_IMAGE)
 #creating the composite map object
 comp_map = sunpy.map.Map(aia_map, hmi_map, composite = True)
 
-#drawing the countours over the hmi_map
-#note that the hmi_map have index = 1
+#drawing the countours over the hmi_map note that the hmi_map have index = 1
 #according to our definition of comp_map
 comp_map.set_levels(index = 1, levels = [-1000,-500,-250,250,500,1000])
 
 #having a look at the map
 comp_map.peek()
 
-#It is also possible to set up the
-#alpha-channel value for a layer in 
-#the CompositeMap.
+#It is also possible to set up the alpha value for a layer in the CompositeMap.
 comp_map.set_alpha(index = 1, alpha = 0.5)
 
 #having another look at the map
