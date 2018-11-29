9495b0f474e2b873ae8d1d94f68f687373c8ddc1
Andre Chicrala <andrechicrala@northumbria.ac.uk>
2018-11-29 16:32:01 +0000

    Changes made to address the sugestions.

--- examples/map/composite_map_AIA_HMI.py
+++ examples/map/composite_map_AIA_HMI.py
@@ -7,19 +7,6 @@ In this example we make a composite map out of images provided by AIA and HMI.
 AIA images are looking at the Sun's Corona intensity at a given temperature
 while an HMI image will measure the magnetic field (in this case the 
 Line-Of-Sight component) at photospheric level.
-
-This procedure consist into taking a pair of images and plot features of one
-of the images on the top of the other. Therefore, we can see on the coronal
-structures present on AIA images what is the field associated to them. 
-
-To filter what magnetic structures we want to see represented on our map we will
-use the levels to define what contours will be draw which, in this case, it 
-will be selected contours ranging from a few hundred to a thousand Gauss which
-is the tipicall field associated to the umbral regions of Active Regions. If 
-the contour values include numbers of the order 1e1 - 1e2 Gauss quiet Sun 
-regions would also be ploted on the top of the original map which in practice 
-would fill the original AIA map with magnetic field readings. 
-
 """
 
 ##############################################################################
@@ -30,19 +17,25 @@ import sunpy.data.sample
 
 ##############################################################################
 # Sunpy sample data contains a number of suitable maps, for this example we
-# will use both AIA 171 and HMI magnetogram
+# will use both AIA 171 and HMI magnetogram.
 aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 
 hmi_map = sunpy.map.Map(sunpy.data.sample.HMI_LOS_IMAGE)
 
-#creating the composite map object
+# Creating the composite map object. This procedure consist into taking a pair 
+# of images and plot features of one of the images on the top of the other.
 comp_map = sunpy.map.Map(aia_map, hmi_map, composite=True)
 
-#drawing the countours over the hmi_map note that the hmi_map have index = 1
-#according to our definition of comp_map
-comp_map.set_levels(index=1, levels=[-1000,-500,-250,250,500,1000])
+# Drawing the countours over the hmi_map note that the hmi_map have index = 1
+# according to our definition of comp_map. We will filter contours ranging 
+# from a few hundred to a thousand Gauss which is the tipical field associated 
+# to umbral regions of Active Regionss.
+comp_map.set_levels(index = 1, levels = [-1000, -500, -250, 250, 500, 1000])
 
-#having a look at the map
+# Having a look at the map. Notice that we can see on the coronal structures 
+# present on AIA images what is the magnetic field associated to them.
 comp_map.peek()
 
-##############################################################################
+# If the contour values had included numbers of the order 1e1 - 1e2 Gauss quiet 
+# Sun regions would also be ploted on the top of the original map which in 
+# practice would fill the original AIA map with magnetic field readings.
