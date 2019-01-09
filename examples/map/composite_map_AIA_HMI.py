78074f745251e776028069abe40a7245cbdfbe8d
Nabil Freij <nabil.freij@gmail.com>
2019-01-09 10:48:19 +0000

    tided up the files

--- examples/map/composite_map_AIA_HMI.py
+++ examples/map/composite_map_AIA_HMI.py
@@ -1,41 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-=============
-Composite map
-=============
+========================
+Creating a Composite map
+========================
+
 In this example we make a composite map out of images provided by AIA and HMI.
-AIA images are looking at the Sun's Corona intensity at a given temperature
-while an HMI image will measure the magnetic field (in this case the 
-Line-Of-Sight component) at photospheric level.
+AIA images look at the solar corona at a given temperature while an HMI image will
+measure the line of sight magnetic field at the photospheric level.
 """
 
-##############################################################################
 # Start by importing the necessary modules.
-
 import sunpy.map
 import sunpy.data.sample
 
 ##############################################################################
-# Sunpy sample data contains a number of suitable maps, for this example we
-# will use both AIA 171 and HMI magnetogram.
-aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
+# Sunpy's sample data contains a selection of data
+# which we will use for this example
 
+aia_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
 hmi_map = sunpy.map.Map(sunpy.data.sample.HMI_LOS_IMAGE)
 
-# Creating the composite map object. This procedure consist into taking a pair 
-# of images and plot features of one of the images on the top of the other.
+##############################################################################
+# Now we will create the composite map which is a set of images stacked upon
+# each other. To do this, we have to call `~sunpy.map.Map` with
+# an extra keyword.
+
 comp_map = sunpy.map.Map(aia_map, hmi_map, composite=True)
 
-# Drawing the countours over the hmi_map note that the hmi_map have index = 1
-# according to our definition of comp_map. We will filter contours ranging 
-# from a few hundred to a thousand Gauss which is the tipical field associated 
-# to umbral regions of Active Regionss.
+##############################################################################
+# Now we want to see what coronal features overlap with regions of strong
+# line of sight magnetic field. So we need to set the contour levels of
+# our composite map using `.set_levels`.
+
+# We want to contour the HMI map, which is the second image in our composite map.
+# Therefore the index is 1.
+# We will filter contours ranging from a few hundred to a thousand Gauss which
+# is the typical field associated to umbral regions of Active Regions.
 comp_map.set_levels(index = 1, levels = [-1000, -500, -250, 250, 500, 1000])
 
-# Having a look at the map. Notice that we can see on the coronal structures 
-# present on AIA images what is the magnetic field associated to them.
-comp_map.peek()
+##############################################################################
+# Now let us look at the result. Notice that we can see the coronal structures
+# present on the AIA image and how they correspond to the line of sight
+# magnetic field.
 
-# If the contour values had included numbers of the order 1e1 - 1e2 Gauss quiet 
-# Sun regions would also be ploted on the top of the original map which in 
-# practice would fill the original AIA map with magnetic field readings.
+comp_map.peek()
--- examples/plotting/hmi_LOSmag.py
+++ examples/plotting/hmi_LOSmag.py
@@ -1,66 +1,53 @@
 """
 ====================
-HMI plotting example
+Creating a HMI image
 ====================
-In this example it is going to be demonstrated how to download and plot data 
-from HMI. For this example we will use the LOS_magnetic_field but any other HMI
-product can be similarly be accessed just by changing the Physical Observables
-(PHYSOBS) keyword on the search. 
 
-Useful links:
-The list with HMI keys for PHYSOBS_.
-.. _PHYSOBS: https://sdac.virtualsolar.org/cgi/show_details?keyword=PHYSOBS
-
-Finding and downloading data using fido_.
-.. _fido: https://docs.sunpy.org/en/stable/guide/acquiring_data/fido.html#downloading-data
-
-Jsoc and VSO attributes for searches and filtering searches_.
-.. _searches: https://docs.sunpy.org/en/stable/code_ref/net.html#
+In this example, we will demonstrate how to download and plot
+line of sight magnetic field data from HMI.
 """
-###############################################################################
-#Importing the required modules.
-import matplotlib.pyplot as plt
+# Start by importing the necessary modules.
 import sunpy.map
 from sunpy.net import Fido, attrs as a
 import astropy.units as u
 
 ###############################################################################
-# Downloading data with fido
-# For this example we will specify time range, sample, instrument and the 
-# Observation type. If you want however to further refine your seach you can 
-# check other attributes that can be used in a Fido search_. 
-# .. _search: https://docs.sunpy.org/en/stable/code_ref/net.html#module-sunpy.net.vso.attrs
-# We will set a time range from 2015/11/04 12:00:00' to '2015/11/04 13:00:00 for
-# HMI LOS magnetic field where our images will be spaced every 720 seconds.
-result = Fido.search(a.Time('2015/11/04 12:00:00', '2015/11/04 13:00:00'),
+# Now we will download some data with `sunpy.net.Fido`.
+# A `Fido.search` requires us to specify a `~sunpy.net.attr.Time`,
+# `~sunpy.net.attr.Sample`, `~sunpy.net.attr.Instrument`
+# and the `~sunpy.net.attr.vso.Physobs`.
+
+# We set a time range from ``2015/11/04 12:00:00`` to ``2015/11/04 12:10:00``
+# for HMI ``LOS_magnetic_field`` with the images spaced every 720 seconds.
+result = Fido.search(a.Time('2015/11/04 12:00:00', '2015/11/04 12:10:00'),
                      a.Instrument('hmi'),
                      a.Sample(720*u.s),
                      a.vso.Physobs('LOS_magnetic_field'))
 
-# checking what results we obtained from our search
+###############################################################################
+# Now we can see what results we obtained from our search.
 print(result)
 
-# Once we are happy with the results obtained from the search we can use them to
-# download the data with Fido.fetch . Each file will be stored in the directory
-# defined by the path and they will be named with the filename {file} obtained
-# in our results.
-downloaded_files = Fido.fetch(result, path = '/APath/{file}.fits')
+###############################################################################
+# Once we are happy with the results obtained from the search.
+# We can download the data with `Fido.fetch` .
+# In this case we only want one file so we can index the result.
 
-# The downloaded_files variables returns a list with the path for each file 
-# that was downloaded as we can see by printing the variable.
+# Notice we have two files. One is the full disk image we plan to display
+# and the other is a synoptic version of said image.
+downloaded_files = Fido.fetch(result)
+
+###############################################################################
+# The ``downloaded_files`` variables returns a list with the path for the file
+# that was downloaded.
 print(downloaded_files)
 
 ###############################################################################
-# Now we are going to create a sunpy.Map object out of one of our results
+# Now we are going to create a `sunpy.map.Map` object out of the first result.
+
 hmi_map = sunpy.map.Map(downloaded_files[0])
 
 ###############################################################################
-# Once we have the map object plotting it is quite simple.
-# First we define a figure object and an axes for it.
-fig, ax = plt.subplots()
+# Once we have created our Map, we can plot it quite simply doing:
 
-# Now we plot our map on the axes we created
-hmi_map.plot(axes = ax)
-
-# Finally we can see our map
-plt.show()
+hmi_map.peek()
